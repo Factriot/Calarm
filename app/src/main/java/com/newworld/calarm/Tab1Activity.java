21244a71f708ff1a137712c1181cf8841bfbc65c
Factriot <dldmsgus2@naver.com>
2017-06-10 21:34:54 +0900

    2016-06-05 처음에 앱을 켰을 때는 데이터 베이스가 비어있으니깐 거기서 문제가 생길 것 같아서 db가 비어있을 때의 레이아웃이 켜지도록 하려 했으나, 생각한대로 Frame의 내용이 바뀌지 않음. 또 한가지 이상한 점은 DB에 저장된 값이 프로그램을 다시 빌드해도 있는 것 같다는 것이다. Tab1Activity에 simple_list_item_1을 사용할 때는 이전까지 한 알람 값들이 모두 저장되 있지만(리스트뷰에 보임) 그냥 새로운 커스텀 어뎁터를 사용하려 했을 때는 NullPointerException이 난 걸로보아 저장되있지 않다고 보여지기때문이다. 일단은 알람이 왔을때 해당알람을 찾아서 알람을 해체하고 DB에서 값을 변경하는 것을 할 계획이다.

--- app/src/main/java/com/newworld/calarm/Tab1Activity.java
+++ app/src/main/java/com/newworld/calarm/Tab1Activity.java
@@ -3,25 +3,33 @@ package com.newworld.calarm;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static com.newworld.calarm.MainActivity.dbManager;
 
 public class Tab1Activity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        /*
+
         ArrayList<String> LIST_MENU = dbManager.dragData();
-        if(LIST_MENU.isEmpty())Log.d("Tab1","LIST_MENU is empty");
+        if(LIST_MENU.isEmpty()) Log.d("Tab1","LIST_MENU is empty");
         View view = inflater.inflate(R.layout.activity_tab1, null);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.activity_list_item, LIST_MENU);
-        //ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU);
+        //ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.activity_list_item, LIST_MENU);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU);
         ListView listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
-        */
-        return inflater.inflate(R.layout.activity_tab1,container,false);
-        //return view;
+
+
+        //return inflater.inflate(R.layout.activity_tab1,container,false);
+        return view;
     }
 }
